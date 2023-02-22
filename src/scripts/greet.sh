eb4d4bacb852218c8b55175d1854c0979bc439c6
Your Name <you@example.com>
2023-02-21 19:49:36 -0600

    greet.sh

--- src/scripts/greet.sh
+++ src/scripts/greet.sh
@@ -1,21 +1,2 @@
-version: 2.1
-
-jobs:
-  osv-scan:
-    docker:
-      - image: cimg/go:1.20.1
-    steps:
-      - checkout
-      - run:
-          name: Install OSV CLI 
-          command: |
-            go install github.com/google/osv-scanner/cmd/osv-scanner@v1
-      - run:
-          name: Scan for OS Vulnerabilities 
-          command: |
-            osv-scanner -- .
-
-workflows:
-  scan:
-    jobs:
-      - osv-scan
+#!/bin/bash
+echo Hello "${PARAM_TO}"
