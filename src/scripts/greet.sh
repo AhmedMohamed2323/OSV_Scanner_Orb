8eeb91d2f94232806cb95df5994db2a95fa8810a
Your Name <you@example.com>
2023-02-22 09:38:39 -0600

    script/greet command

--- src/scripts/greet.sh
+++ src/scripts/greet.sh
@@ -1,2 +1,17 @@
 #!/bin/bash
 echo Hello "${PARAM_TO}"
+
+commands:
+  osv-scan:
+    parameters:
+      path:
+        type: string
+        default: '.'
+    steps:
+      - run:
+          name: Install OSV CLI
+          command: go install github.com/google/osv-scanner/cmd/osv-scanner@v1
+      - run:
+          name: Scan for OS vulnerabilities
+          command: osv-scanner -- .
+
