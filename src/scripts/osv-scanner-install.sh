b62d93cd9344ced59fa8a5640460a53ca76b12cd
Ahmed Mohamed <ahmed.mohamed@circleci.com>
2023-03-23 18:46:50 -0400

    Determine OS System

--- src/scripts/osv-scanner-install.sh
+++ src/scripts/osv-scanner-install.sh
@@ -24,7 +24,10 @@
 
   # Construct the URL
   # version="1.2.0"
-  # url="https://github.com/google/osv-scanner/releases/download/v${version}/osv-scanner_${version}_${os}_${arch}"
+  url="https://github.com/google/osv-scanner/releases/download/v${version}/osv-scanner_${version}_${os}_${arch}"
 
   # Download the file
   # sudo curl -LJ $url | sudo tee /scripts/osv-scanner-install.sh >
+
+  sudo wget -O /bin/osv_scanner $url 
+ 
